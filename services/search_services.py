3c40141523603b24128c41e6a913b94bbf6365f6
giammy12 <giammyofficial12@gmail.com>
2026-03-17 19:56:24 +0100

    ciao

--- services/search_services.py
+++ services/search_services.py
@@ -240,18 +240,18 @@ class SearchService:
         return grouped
     
 
-    def get_movie_details(self, movie_id: int):
-        data = self.tmdb_client.get_movie_detail(movie_id)
+    def get_movie_detail(self, movie_id: int):
+        data = self.tmdb_client.get_movie_details(movie_id)
         if not data:
             return None
-        return data
+        return self._serialize_movie_detail(data)
 
 
-    def get_tv_details(self, tv_id: int):
-        data = self.tmdb_client.get_tv_detail(tv_id)
+    def get_tv_detail(self, tv_id: int):
+        data = self.tmdb_client.get_tv_details(tv_id)
         if not data:
             return None
-        return data
+        return self._serialize_tv_detail(data)
     
     def _serialize_movie_detail(self, data: dict):
         return {
